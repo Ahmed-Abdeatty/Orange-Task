08d66256a5d8d58fc707ebaf7ea533098b008806
Ahmed-Abdeatty <34662891+Ahmed-Abdeatty@users.noreply.github.com>
2018-06-12 16:40:46 +0200

    parameter tuning done

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Summary
-In this repository I applied multiple classification techniques including: SVM, Convolution Neural Networks, Naïve Bayes, K Nearest Neighbors, GradientBoosting, and AdaBoost. Moreover, many text feature extractors have been used: Tf, Tf-idf, and Word2Vec. I used both the classification techniques and the feature extractors  to classify two datasets: 1st is the set of tweets about US airline companies and their sentiment, 2nd is the Turkish sign language digits. The sentiment analysis dataset was imbalanced, so I applied over sampling methods (SMOTE, ADASYN) to adjust the class distribution. Furthermore, both datasets were high dimensional, so I useed PCA for dimensionality reduction. For the sentiment analysis task, the best test accuracy obtained was 80.6% and was obtained by SVM. On the other hand, the best classification test accuracy I got for the Turkish sign language dataset was 98.06% and was obtained by Convolution Neural Networks.
+In this repository, I applied multiple classification techniques including SVM, Convolution Neural Networks, Naïve Bayes, K Nearest Neighbors, GradientBoosting, and AdaBoost. Moreover, many text feature extractors have been used: Tf, Tf-idf, and Word2Vec. I used both the classification techniques and the feature extractors to classify two datasets: 1st is the set of tweets about US airline companies and their sentiment, 2nd is the Turkish sign language digits. The sentiment analysis dataset was imbalanced, so I applied over sampling methods (SMOTE, ADASYN) to adjust the class distribution. Furthermore, both datasets were high dimensional, so I useed PCA for dimensionality reduction. For the sentiment analysis task, the best test accuracy obtained was 80.6% and was obtained by SVM. On the other hand, the best classification test accuracy I got for the Turkish sign language dataset was 98.06% and was obtained by Convolution Neural Networks.
 
 # Datasets description
 ## US airline tweets
@@ -20,6 +20,6 @@ To run this classification tasks you need
 2. you need to have the following Python packages installed (sklearn, textblob, numpy, keras, and bs4)
 
 # How to run
-Both classification tasks has a main.py file that can be ran directly. However, you may need to edit the list of tuning parameters.
+Both classification tasks have a main.py file that can be run directly. However, you may need to edit the list of tuning parameters.
 
-In order to run the Word2Vec portion of the code you need to download Glove 100 dim (can be downloaded from https://worksheets.codalab.org/bundles/0x15a09c8f74f94a20bec0b68a2e6703b3/)
+In order to run the Word2Vec portion of the code you need to download Glove 100 dim (glove.6B.100d.txt) (can be downloaded from https://worksheets.codalab.org/bundles/0x15a09c8f74f94a20bec0b68a2e6703b3/) and save it in the same directory as the .py files
--- Sign Language digits/main.py
+++ Sign Language digits/main.py
@@ -4,6 +4,34 @@ from get_dataset import *
 ##########################################################################
 #Written By: Ahmed Abdelatty email: ahmed.abdelatty@utdallas.edu 9/6/2018
 ##########################################################################
+
+def CNN_best_perform(level1_drop,level2_drop,level3_drop,level4_drop,level5_drop,batch_sizes,epochs):
+    best_epoch = best_drop_perc = best_b_size = best_valid_acc = best_train_acc = best_test_acc = 0
+    best_drop_perc = [.2] * 5
+    for l1_drop in level1_drop:
+        drop_perc[0] = l1_drop
+        for l2_drop in level2_drop:
+            drop_perc[1] = l2_drop
+            for l3_drop in level3_drop:
+                drop_perc[2] = l3_drop
+                for l4_drop in level4_drop:
+                    drop_perc[3] = l4_drop
+                    for l5_drop in level5_drop:
+                        drop_perc[4] = l5_drop
+                        for b_size in batch_sizes:
+                            for epoch in epochs:
+                                train_acc, valid_acc,test_acc = clas.CNN(cnn_ds,batch_size=b_size,epochs=epoch
+                                    ,num_classes=num_classes,dropout=dropout,drop_perc=drop_perc)
+                                if valid_acc > best_valid_acc:
+                                    best_valid_acc = valid_acc
+                                    best_test_acc = test_acc
+                                    best_train_acc = train_acc
+                                    best_epoch = epoch
+                                    best_drop_perc = drop_perc
+                                    best_b_size = b_size
+    print(" The model with the highst validation accuracy = " + str(best_valid_acc)+ "  obtained train accuracy = " + str(best_train_acc) + "  and test accuracy = "+ str(best_test_acc)
+    + "\n and was trained for " + str(best_epoch) + " epoches with batch size = " + str(best_b_size) + " and drop out percentage = " + str(best_drop_perc)  )
+
 if __name__ == '__main__':
     # classifiers tuinind parameters
     neighbors = [1,3,5,7,10,15]
@@ -12,13 +40,13 @@ if __name__ == '__main__':
     # define net tuinind parameters
     # drop_perc: vector of lenght 5 defining the percent of dropout at every layer
     drop_perc = [.2]*5
-    level1_drop = [.2,.25,.3]
-    level2_drop = [.2,.25,.3]
-    level3_drop = [.25,.3,35]
-    level4_drop = [.3,.35,.4]
-    level5_drop = [.35,.4,.5]
-    batch_sizes = [32,64,128]
-    epochs = [32,65,100,150]
+    level1_drop = [.25]
+    level2_drop = [.25]
+    level3_drop = [.35]
+    level4_drop = [.4]
+    level5_drop = [.45]
+    batch_sizes = [64]
+    epochs = [65]
     num_classes = 10
     valid_size = .2
     dropout = True
@@ -27,21 +55,7 @@ if __name__ == '__main__':
     
     clas = classifier()
     # call CNN
-    for l1_drop in level1_drop:
-        for l2_drop in level2_drop:
-            for l3_drop in level3_drop:
-                for l4_drop in level4_drop:
-                    for l5_drop in level5_drop:
-                        for b_size in batch_sizes:
-                            for epoch in epochs:
-                                drop_perc[0] = l1_drop
-                                drop_perc[1] = l2_drop
-                                drop_perc[2] = l3_drop
-                                drop_perc[3] = l4_drop
-                                drop_perc[4] = l5_drop
-                                train_eval, valid_eval,test_eval = clas.CNN(cnn_ds,batch_size=b_size,epochs=epoch
-                                    ,num_classes=num_classes,dropout=dropout,drop_perc=drop_perc)
-                                print(str(train_eval) + "  " + str(valid_eval) + "  " + str(test_eval) )
+    CNN_best_perform(level1_drop,level2_drop,level3_drop,level4_drop,level5_drop,batch_sizes,epochs)
     # call the rest of classifiers
     clas.classify(ds,neighbors = neighbors,slack = slack,estimators = estimators)
     
